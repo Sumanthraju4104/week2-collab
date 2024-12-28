282463f246b08db630b90b777f32b3ab0f310d16
Sumanthraju4104 <sarikondasumanthraju@gmail.com>
2024-12-28 17:52:22 +0530

    week2-up

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-
+const validator = require('validator');
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true,
+    minlength: [2, 'Name must be at least 2 characters long'],
+    maxlength: [50, 'Name must be less than 50 characters long']},
+  email: { type: String, required: true, unique: true, lowercase: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide a valid email',
+    } },
   password: { type: String, required: true },
 });
 
-//for encrypting the password feild before saving into db
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) {
       next();
@@ -15,7 +20,7 @@ userSchema.pre('save', async function(next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   console.log('Hashed Password during Registration:', this.password);
-  next();  // Ensure that next() is called after hashing
+  next();  
 });
 
 module.exports = mongoose.model('User', userSchema);
